1cc96b0d2f967642f41fcae114fb7c4fc642fbf6
ESUAdmin <cla2000512@tuta.io>
2020-09-30 22:54:17 +0800

    Optimize requests to serverless

--- cf-worker.js
+++ cf-worker.js
@@ -1,23 +1,98 @@
-const url = "https://media.githubusercontent.com/media/ESUAdmin/weibo-leaks/master/bloom-filter-data.bin";
+const dataUrl = "https://media.githubusercontent.com/media/ESUAdmin/weibo-leaks/master/bloom-filter-data.bin";
+const allowOrigin = "https://esuadmin.github.io";
 
 addEventListener("fetch", event => {
     event.respondWith(handler(event.request));
 });
 
+const magicNumbers = [0, 810, 893, 1919, 114514, 1919810];
+// don't change BigInt type
+const byteOffset = 8n; // sizeof std::size_t
+const maxBytes = 512892944;
+const bitSize = 4103143449n;
+// FNV-1a 64bit
+const fnv_prime_64 = 1099511628211n;
+const fnv_offset_basis = 14695981039346656037n;
+
+// array of i -> bi
+function fnv_1a_64(bytes) {
+    let hash = fnv_offset_basis;
+    for (let byte of bytes) {
+        hash = ((hash ^ BigInt(byte)) * fnv_prime_64) & 0xFFFFFFFFFFFFFFFFn;
+    }
+    return hash;
+}
+
+// bi -> i
+async function getNthByte(fileByteIndex) {
+    const partIndex = Number(fileByteIndex % 1024n);
+    const begin = Number((fileByteIndex / 1024n) * 1024n);
+    let end = begin + 1023;
+    if (end >= maxBytes) end = maxBytes - 1;
+
+    const response = await fetch(dataUrl, {
+        "headers": {
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "range": `bytes=${begin}-${end}`,
+        },
+        "body": null,
+        "method": "GET",
+    });
+    const bytes = new Uint8Array(await response.arrayBuffer());
+    return bytes[partIndex];
+}
+
+// bi -> 01
+async function getBloomBit(bitIndex) {
+    const byteIndex = byteOffset + (bitIndex >> 3n);
+    const inner = Number(bitIndex % 8n);
+    return (await getNthByte(byteIndex) >> inner) & 1;
+}
+
+// bi -> array of i
+function bi2bytes(big) {
+    const bytes = [];
+    while (big > 0n) {
+        bytes.push(Number(big % 256n));
+        big >>= 8n;
+    }
+    return bytes.concat(new Array(8 - bytes.length).fill(0));
+}
+
+// i, i -> i
+function mixHash(uid, p) {
+    return fnv_1a_64(
+        bi2bytes(((BigInt(p) << 32n) | BigInt(uid)))
+    ) % bitSize;
+}
+
+// i -> bool
+async function queryBloom(uid) {
+    for (const magic of magicNumbers) {
+        if (await getBloomBit(mixHash(uid, magic)) === 0) return false;
+    }
+    return true;
+}
+
 async function handler(request) {
+    const url = request.url;
+    const uid = parseInt(url.substr(url.lastIndexOf("/") + 1));
     let response;
-    if (!request.headers.has('Range')) {
-        response = new Response('OK', {status: 200});
+
+    if (isNaN(uid) || uid <= 0) {
+        response = new Response("Invalid argument", {status: 400});
     } else {
         try {
-            response = await fetch(url, request);
-            response = new Response(response.body, response);
-        } catch (err) {
-            return new Response("Bad Request", {status: 400});
+            const result = await queryBloom(uid);
+            response = new Response(String(result), {status: 200});
+        } catch (e) {
+            response = new Response("Server error", {status: 500});
         }
     }
-    /* i ee, grass */
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Headers', '*');
+    response.headers.set('Access-Control-Allow-Origin', allowOrigin);
+
     return response;
 }
--- main.js
+++ main.js
@@ -1,92 +1,37 @@
 (function () {
     'use strict';
-    const bloomDataURL = "https://weibo.eswk.workers.dev/";
-    const magicNumbers = [0n, 810n, 893n, 1919n, 114514n, 1919810n];
-    const byteOffset = 8n; // sizeof std::size_t
-    const maxBytes = 512892944n;
-    const bitSize = 4103143449n;
+    // init materialize
+    document.addEventListener('DOMContentLoaded', function() {
+        let elems = document.querySelectorAll('.sidenav');
+        let instances = M.Sidenav.init(elems, {});
+    });
+
+    const apiUrl = "https://weibo-leaks.eswk.workers.dev/";
     const trueRate = Math.round(1000000 / 102) / 100;
 
-    const fnv_prime_64 = 1099511628211n;
-    const fnv_offset_basis = 14695981039346656037n;
-
-    function fnv_1a_64(bytes) {
-        let hash = fnv_offset_basis;
-        for (let byte of bytes) {
-            hash = ((hash ^ BigInt(byte)) * fnv_prime_64) & 0xFFFFFFFFFFFFFFFFn;
-        }
-        return hash;
-    }
-
-    async function getNthByte(fileByteIndex) {
-        console.log(fileByteIndex);
-        const partIndex = fileByteIndex % 1024n;
-        const begin = (fileByteIndex / 1024n) * 1024n;
-        let end = begin + 1023n;
-        if (end >= maxBytes) end = maxBytes - 1n;
-
-        console.log(begin, end, partIndex);
-        const response = await fetch(bloomDataURL, {
-            "headers": {
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "none",
-                "sec-fetch-user": "?1",
-                "range": `bytes=${Number(begin)}-${Number(end)}`,
-            },
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "credentials": "omit"
-        });
-        const bytes = new Uint8Array(await response.arrayBuffer());
-        return bytes[Number(partIndex)];
-    }
-
-    async function getBloomBit(bitIndex) {
-        const byteIndex = byteOffset + (bitIndex >> 3n);
-        const inner = Number(bitIndex % 8n);
-        return (await getNthByte(byteIndex) >> inner) & 1;
-    }
-
-    function bi2bytes(big) {
-        const bytes = [];
-        while (big > 0n) {
-            bytes.push(Number(big % 256n));
-            big >>= 8n;
-        }
-        console.log(bytes.concat(new Array(8 - bytes.length).fill(0)));
-        return bytes.concat(new Array(8 - bytes.length).fill(0));
-    }
-
-    function mixHash(uid, p) {
-        return fnv_1a_64(
-            bi2bytes(((p << 32n) | BigInt(uid)))
-        ) % bitSize;
-    }
-
-    async function queryBloom(uid) {
-        for (let number of magicNumbers) {
-            if (await getBloomBit(mixHash(uid, number)) === 0) return false;
-        }
-        return true;
-    }
-
-    document.querySelector("button#chudao").addEventListener('click', async () => {
+    document.querySelector("button#chudao").addEventListener("click", async () => {
         const resultText = document.querySelector("p#result");
         const uid = parseInt(document.querySelector("input#uid").value);
-        if (isNaN(uid)) {
+        if (isNaN(uid) || uid <= 0) {
             alert("你在干什么？");
             return false;
         }
         resultText.innerText = "正在创象出道中…… 可能需要亿点时间 ……";
-        if (await queryBloom(uid)) {
-            alert("恭喜！已被开盒。");
-            resultText.innerText = `${uid} 有 ${trueRate}% 概率已经被出道，，，`;
+        const response = await fetch(apiUrl + String(uid));
+        const text = await response.text();
+        if (response.status === 200) {
+            if (text === "true") {
+                alert("恭喜！已被开盒。");
+                resultText.innerText = `${uid} 有 ${trueRate}% 概率已经被出道，，，`;
+            } else if (text === "false") {
+                alert("很好，你没事了。");
+                resultText.innerText = `${uid} 安全，没有记录。`;
+            } else {
+                alert("这不应该发生。");
+            }
         } else {
-            alert("很好，你没事了。");
-            resultText.innerText = `${uid} 安全，没有记录。`;
+            alert(`查询出现问题…… 代码 ${response.status}`);
+            resultText.innerText = await response.text();
         }
-    })
+    });
 })();
